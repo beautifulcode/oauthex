a97a3f4b3fecc6357f2ecc0e77f38cce31cbde10
Aaron Glenn <aaron@aaronglenn.ca>
2014-10-05 11:25:51 -0700

    update package

--- package.exs
+++ package.exs
@@ -3,6 +3,5 @@ Expm.Package.new(
   description: "Wrapper for erlang-oauth",
   version: "0.0.1",
   keywords: ["oauth"],
-  maintainers: [[name: "Marcelo Gornstein", email: "marcelog@gmail.com"]],
-  repositories: [[github: "marcelog/oauthex", tag: "0.0.1"]]
+  repositories: [[github: "beautifulcode/oauthex", tag: "0.0.1"]]
 )
