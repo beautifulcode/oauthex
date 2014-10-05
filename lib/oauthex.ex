6ebb9a3105e5c792777ee8c41d87bda0f2a43247
Aaron Glenn <aaron@aaronglenn.ca>
2014-10-05 10:53:12 -0700

    Update Record syntax and remove Lager dep for the time being.

--- lib/oauthex.ex
+++ lib/oauthex.ex
@@ -5,12 +5,13 @@ defmodule Oauthex do
     params_decode(response)
   ], to: :oauth
 
-  use Application.Behaviour
-  require Lager
+  use Application
+  #require Lager
+  require Record
 
-  defrecord Consumer, key: nil, secret: nil, hash: :hmac_sha1
-  defrecord ReqInfo, token: nil, secret: nil
-  defrecord AccInfo, token: nil, secret: nil
+  Record.defrecord :consumer_instance, Oauthex, key: nil, secret: nil, hash: :hmac_sha1
+  Record.defrecord :req_info, Oauthex, token: nil, secret: nil
+  Record.defrecord :acc_info, Oauthex, token: nil, secret: nil
 
   def start(_, _) do
   end
@@ -18,13 +19,13 @@ defmodule Oauthex do
   def access_token(url, params, consumer, reqinfo) do
     result = get url, params, consumer, reqinfo
     {token, secret} = token_info result
-    AccInfo.new token: token, secret: secret
+    acc_info token: token, secret: secret
   end
 
   def request_token(url, params, consumer) do
     result = get url, params, consumer
     {token, secret} = token_info result
-    ReqInfo.new token: token, secret: secret
+    req_info token: token, secret: secret
   end
 
   def token_info(result) do
@@ -33,7 +34,7 @@ defmodule Oauthex do
   end
 
   def consumer_to_tuple(consumer) do
-    {consumer.key, consumer.secret, consumer.hash}
+    {consumer_instance(consumer, :secret), consumer_instance(consumer, :secret), consumer_instance(consumer, :hash)}
   end
 
   def post(url, params, consumer) do
@@ -43,7 +44,7 @@ defmodule Oauthex do
   def post(url, params, consumer, reqinfo) do
     assert_get :oauth.post(
       url, params, consumer_to_tuple(consumer),
-      reqinfo.token, reqinfo.secret, 
+      reqinfo.token, reqinfo.secret,
       [{:sync, :false}, {:stream, :self}]
     )
   end
@@ -65,11 +66,11 @@ defmodule Oauthex do
   defp assert_get(result) do
     case result do
       {:ok, {{_,200,_},_,_}=data} ->
-        Lager.debug 'Result: ~p', [data];
+        #Lager.debug 'Result: ~p', [data];
         data
       {:ok, ref} -> ref
       result ->
-        Lager.error 'oauth error ~p', [result]
+        #Lager.error 'oauth error ~p', [result]
         raise {:oauth_error, result}
     end
   end
--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Oauthex.Mixfile do
   def application do
     [
       applications: [
-        :sasl, :exlager, :oauth, :crypto, :ssl, :public_key, :inets
+        :sasl, :oauth, :crypto, :ssl, :public_key, :inets
       ]
     ]
   end
@@ -20,8 +20,8 @@ defmodule Oauthex.Mixfile do
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      {:exlager,"~> 0.2.0",[github: "khia/exlager"]},
-      {:oauth, "1.4.0", [github: "tim/erlang-oauth"]}
+      #{:exlager,"~> 0.3",[github: "khia/exlager"]},
+      {:oauth, "~> 1.4", [github: "tim/erlang-oauth"]}
     ]
   end
 end
